ec04850623a83880a868d2036afd18a0ae565c98
Shannon <shaw0106@algonquinlive.com>
2016-03-31 08:22:04 -0400

    Vars done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 var $body = $('body');
 
-var $btnShowHide = $('$btnShowHide');
+var $btnShowHide = $('$btn-show-hide');
 var $box = $('.box');
+
+var $btnMove = $('.btnMove');
+var $diamond = $('.diamond');
+
+var $btnColEx = $('.btnColEx');
+var $panel = $('.panel');
+
+var $btnBounce = $('.btnBounce');
+var $circle = $('.circle');
+
+var $btnAppend = $('.btnAppend');
+var $list = $('.list')
