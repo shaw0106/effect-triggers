5ef49e16da0dced9cdbb6636449b62af22103190
Shannon <shaw0106@algonquinlive.com>
2016-03-31 08:22:59 -0400

    fixed vars

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ var $body = $('body');
 var $btnShowHide = $('$btn-show-hide');
 var $box = $('.box');
 
-var $btnMove = $('.btnMove');
+var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
-var $btnColEx = $('.btnColEx');
+var $btnColEx = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
-var $btnBounce = $('.btnBounce');
+var $btnBounce = $('.btn-bounce');
 var $circle = $('.circle');
 
-var $btnAppend = $('.btnAppend');
+var $btnAppend = $('.btn-append');
 var $list = $('.list')
